46944f7a3a99355bf67f48171a738da9f8dd85b5
Min_h_bot <min_h_bot@Min-h-botui-MacBookPro.local>
2024-07-16 16:31:07 +0900

    init : 선수 입장~

---
+++ src/test/java/com/project/woomool/WoomoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.woomool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoomoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
