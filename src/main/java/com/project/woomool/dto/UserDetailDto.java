0db439cb14628b78ebd5ac86aedb46deb7fc1482
kai <lucas0606@handong.ac.kr>
2024-07-20 15:39:39 +0900

    fix: userdetail에서 hasdrank 뺌

--- src/main/java/com/project/woomool/dto/UserDetailDto.java
+++ src/main/java/com/project/woomool/dto/UserDetailDto.java
@@ -14,14 +14,12 @@ public class UserDetailDto {
     private float bmi;
     private float recommendation;
     private float todayTotal;
-    private byte hasDrank;
 
     public static UserDetailDto of(UserDetail userDetail) {
         UserDetailDto dto = new UserDetailDto();
         dto.setWeight(userDetail.getWeight());
         dto.setHeight(userDetail.getHeight());
         dto.setBmi(userDetail.getBmi());
-        dto.setHasDrank(userDetail.getHasDrank());
         dto.setTodayTotal(userDetail.getTodayTotal());
         dto.setRecommendation(userDetail.getRecommendation());
         return dto;
